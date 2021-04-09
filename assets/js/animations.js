a6665572a93f740cb1c46fabdb95c7e0ae852fd7
OlaHulleberg <ola.hulleberg@gmail.com>
2021-04-09 20:10:35 +0200

    Streamline frameContainer, small QoL change

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -2,27 +2,28 @@ function ApplyFrame(frameID)
 {
     disableScroll(); // Disable scroll while animation is on-going.
     var frameDuration = 3000;
+    var frameContainer = $('body');
 
     switch(frameID) {
         case 1:
-            $('body').addClass('frame-nb1');
+            frameContainer.addClass('frame-nb1');
             break;
         
         case 2:
-            $('body').addClass('frame-nb2');
+            frameContainer.addClass('frame-nb2');
             break;
 
         case 3:
-            $('body').addClass('frame-nb3');
+            frameContainer.addClass('frame-nb3');
             break;
 
         case 4:
-            $('body').addClass('frame-nb4');
+            frameContainer.addClass('frame-nb4');
             break;
         
         case 5:
             frameDuration = 5000;
-            $('body').addClass('frame-nb5');
+            frameContainer.addClass('frame-nb5');
             break;
     }
     setTimeout(() => {enableScroll();}, frameDuration);
